a065d4e8c95c91f2bd317d0571ed8791b9094f66
Abigail <abigail.tyrell@noaa.gov>
2021-03-23 18:17:38 -0400

    regression report can be downloaded from app!

--- server.R
+++ server.R
@@ -6,32 +6,49 @@ if (!"NEesp" %in% installed.packages()) {
   devtools::install_github("NOAA-EDAB/esp_data_aggretation@package")
 }
 
-render_reg_report_shiny <- function(stock_var, epus_var  = "MAB", 
+`%>%` <- magrittr::`%>%`
+library(NEesp)
+library(shiny)
+
+render_reg_report_shiny <- function(stock_var, 
+                                    epus_var  = "MAB", 
                                     region_var = "Mid", 
-                                    remove_var = FALSE, file_var,
-                                    lag_var = 0, save_var = TRUE, 
-                                    out = "word") {
+                                    remove_var = FALSE,
+                                    lag_var = 0, 
+                                    save_var = TRUE, 
+                                    out = "word", 
+                                    file_var) {
   starting_dir <- getwd()
   
   new_dir <- tempdir()
   
+  setwd(new_dir)
+  dir.create("BOOK")
+  setwd("BOOK")
+  this_dir <- getwd()
+  
+  # make sure directory is clean
+  existing_files <- list.files(full.names = TRUE, 
+                               recursive = TRUE, 
+                               all.files = TRUE)
+  if(length(existing_files) > 0){
+    file.remove(existing_files)
+  }
+
   file.copy(
       from = list.files(system.file("correlation_bookdown_template", package = "NEesp"),
                         full.names = TRUE
       ),
-      to = here::here(new_dir),
+      to = getwd(),
       overwrite = TRUE
     ) %>%
       invisible()
- 
-  setwd(here::here(new_dir))
-  
+
   if(save_var){
     dir.create("data",
                recursive = TRUE
     )
   }
-  
   
   # render bookdown
     bookdown::render_book(
@@ -41,31 +58,43 @@ render_reg_report_shiny <- function(stock_var, epus_var  = "MAB",
         stock = stock_var,
         region = region_var,
         epu = c(epus_var, c("All", "all", "NE")),
-        path = here::here(new_dir, "figures//"),
+        path = paste(this_dir, "/figures//", sep = ""),
         save = save_var,
-        remove_recent = remove_var
+        remove_recent = remove_var,
+        out = out
       ),
       output_format = bookdown::word_document2(),
       envir = new.env(parent = globalenv()),
       output_file = file_var,
+      output_dir = this_dir,
+      intermediates_dir = this_dir,
+      knit_root_dir = this_dir,
       clean = TRUE,
       quiet = FALSE
     )
-  
 
-  setwd(starting_dir)
-
+    # zip files
+    files2zip <- c(list.files(full.names = TRUE,
+                              recursive = TRUE,
+                              pattern = "report.doc"),
+                   list.files(full.names = TRUE,
+                              recursive = TRUE,
+                              pattern = ".png"),
+                   list.files(full.names = TRUE,
+                              recursive = TRUE,
+                              pattern = ".csv"))
+    
+    print(files2zip)
+    
+    utils::zip(zipfile = 'testZip', files = files2zip)
+    
 }
-
-`%>%` <- magrittr::`%>%`
-library(NEesp)
-library(shiny)
 
 server <- function(input, output){
   
   output$report <- downloadHandler(
     # For PDF output, change this to "report.pdf"
-    filename = "report.doc",
+    filename = function() {"report.zip"},
     content = function(file) {
       # Copy the report file to a temporary directory before processing it, in
       # case we don't have write permissions to the current working dir (which
@@ -74,13 +103,27 @@ server <- function(input, output){
       # Set up parameters to pass to Rmd document
       #params <- list(n = input$slider)
       
+      id <- showNotification(
+        "Rendering report...", 
+        duration = NULL, 
+        closeButton = FALSE
+      )
+      on.exit(removeNotification(id), add = TRUE)
+      
       # Knit the document, passing in the `params` list, and eval it in a
       # child of the global environment (this isolates the code in the document
       # from the code in this app).
       render_reg_report_shiny(stock_var = input$species,
-                              file_var = file,
-                              save_var = FALSE)
+                              epus_var  = input$epu, 
+                              region_var = input$region, 
+                              remove_var = input$remove,
+                              lag_var = as.numeric(input$lag), 
+                              file_var = "report.docx",
+                              save_var = TRUE)
 
-    }
+      file.copy("testZip.zip", file)
+
+    },
+    contentType = "application/zip"
   )
 }
--- ui.R
+++ ui.R
@@ -3,5 +3,21 @@ ui <- fluidPage(
               label = "Species",
               choices = NEesp::species_key$Species),
   
+  selectInput(inputId = "epu",
+              label = "EPU",
+              choices = c("GB", "GOM", "MAB")),
+  
+  selectInput(inputId = "region",
+              label = "Stock Region",
+              choices = sort(unique(NEesp::regression_species_regions$Region))),
+  
+  selectInput(inputId = "lag",
+              label = "Lag correlation? \n(for predictive potential)",
+              choices = 0:10),
+  
+  selectInput(inputId = "remove",
+              label = "Remove past 10 years of data? \n(for predictive potential)",
+              choices = c("FALSE", "TRUE")),
+  
   downloadButton("report", "Generate report")
 )

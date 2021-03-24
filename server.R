733682ae8c9648a2f6309c6045880fd1f5cd5b66
Abigail <abigail.tyrell@noaa.gov>
2021-03-24 11:38:10 -0400

    update app

--- server.R
+++ server.R
@@ -10,120 +10,129 @@ if (!"NEesp" %in% installed.packages()) {
 library(NEesp)
 library(shiny)
 
-render_reg_report_shiny <- function(stock_var, 
-                                    epus_var  = "MAB", 
-                                    region_var = "Mid", 
+render_reg_report_shiny <- function(stock_var,
+                                    epus_var = "MAB",
+                                    region_var = "Mid",
                                     remove_var = FALSE,
-                                    lag_var = 0, 
-                                    save_var = TRUE, 
-                                    out = "word", 
+                                    lag_var = 0,
+                                    save_var = TRUE,
+                                    out = "word",
                                     file_var) {
   starting_dir <- getwd()
-  
+
   new_dir <- tempdir()
-  
+
   setwd(new_dir)
   dir.create("BOOK")
   setwd("BOOK")
   this_dir <- getwd()
-  
+
   # make sure directory is clean
-  existing_files <- list.files(full.names = TRUE, 
-                               recursive = TRUE, 
-                               all.files = TRUE)
-  if(length(existing_files) > 0){
+  existing_files <- list.files(
+    full.names = TRUE,
+    recursive = TRUE,
+    all.files = TRUE
+  )
+  
+  if (length(existing_files) > 0) {
     file.remove(existing_files)
   }
 
   file.copy(
-      from = list.files(system.file("correlation_bookdown_template", package = "NEesp"),
-                        full.names = TRUE
-      ),
-      to = getwd(),
-      overwrite = TRUE
-    ) %>%
-      invisible()
+    from = list.files(system.file("correlation_bookdown_template", package = "NEesp"),
+      full.names = TRUE
+    ),
+    to = getwd(),
+    overwrite = TRUE
+  ) %>%
+    invisible()
 
-  if(save_var){
+  if (save_var) {
     dir.create("data",
-               recursive = TRUE
+      recursive = TRUE
     )
   }
-  
-  # render bookdown
-    bookdown::render_book(
-      input = ".",
-      params = list(
-        lag = lag_var,
-        stock = stock_var,
-        region = region_var,
-        epu = c(epus_var, c("All", "all", "NE")),
-        path = paste(this_dir, "/figures//", sep = ""),
-        save = save_var,
-        remove_recent = remove_var,
-        out = out
-      ),
-      output_format = bookdown::word_document2(),
-      envir = new.env(parent = globalenv()),
-      output_file = file_var,
-      output_dir = this_dir,
-      intermediates_dir = this_dir,
-      knit_root_dir = this_dir,
-      clean = TRUE,
-      quiet = FALSE
-    )
 
-    # zip files
-    files2zip <- c(list.files(full.names = TRUE,
-                              recursive = TRUE,
-                              pattern = "report.doc"),
-                   list.files(full.names = TRUE,
-                              recursive = TRUE,
-                              pattern = ".png"),
-                   list.files(full.names = TRUE,
-                              recursive = TRUE,
-                              pattern = ".csv"))
-    
-    print(files2zip)
-    
-    utils::zip(zipfile = 'testZip', files = files2zip)
-    
+  # render bookdown
+  bookdown::render_book(
+    input = ".",
+    params = list(
+      lag = lag_var,
+      stock = stock_var,
+      region = region_var,
+      epu = c(epus_var, c("All", "all", "NE")),
+      path = paste(this_dir, "/figures//", sep = ""),
+      save = save_var,
+      remove_recent = remove_var,
+      out = out
+    ),
+    output_format = bookdown::word_document2(),
+    envir = new.env(parent = globalenv()),
+    output_file = file_var,
+    output_dir = this_dir,
+    intermediates_dir = this_dir,
+    knit_root_dir = this_dir,
+    clean = TRUE,
+    quiet = FALSE
+  )
+
+  # zip files
+  files2zip <- c(
+    list.files(
+      full.names = TRUE,
+      recursive = TRUE,
+      pattern = "report.doc"
+    ),
+    list.files(
+      full.names = TRUE,
+      recursive = TRUE,
+      pattern = ".png"
+    ),
+    list.files(
+      full.names = TRUE,
+      recursive = TRUE,
+      pattern = ".csv"
+    )
+  )
+
+  print(files2zip)
+
+  utils::zip(zipfile = "testZip", files = files2zip)
 }
 
-server <- function(input, output){
-  
+server <- function(input, output) {
   output$report <- downloadHandler(
-    # For PDF output, change this to "report.pdf"
-    filename = function() {"report.zip"},
-    content = function(file) {
-      # Copy the report file to a temporary directory before processing it, in
-      # case we don't have write permissions to the current working dir (which
-      # can happen when deployed).
 
-      # Set up parameters to pass to Rmd document
-      #params <- list(n = input$slider)
-      
+    filename = function() {
+      "report.zip"
+    },
+    content = function(file) {
+
       id <- showNotification(
-        "Rendering report...", 
-        duration = NULL, 
-        closeButton = FALSE
+        "Rendering report...",
+        duration = NULL,
+        closeButton = FALSE,
+        type = "message"
       )
       on.exit(removeNotification(id), add = TRUE)
-      
-      # Knit the document, passing in the `params` list, and eval it in a
-      # child of the global environment (this isolates the code in the document
-      # from the code in this app).
-      render_reg_report_shiny(stock_var = input$species,
-                              epus_var  = input$epu, 
-                              region_var = input$region, 
-                              remove_var = input$remove,
-                              lag_var = as.numeric(input$lag), 
-                              file_var = "report.docx",
-                              save_var = TRUE)
+
+      render_reg_report_shiny(
+        stock_var = input$species,
+        epus_var = input$epu,
+        region_var = input$region,
+        remove_var = input$remove,
+        lag_var = as.numeric(input$lag),
+        file_var = "report.docx",
+        save_var = TRUE
+      )
 
       file.copy("testZip.zip", file)
-
     },
     contentType = "application/zip"
   )
+
+  output$table <- DT::renderDataTable(NEesp::make_html_table_thin(
+    NEesp::regression_species_regions,
+    col_names = colnames(NEesp::regression_species_regions)),
+                                      server = FALSE)
 }
--- ui.R
+++ ui.R
@@ -1,23 +1,77 @@
 ui <- fluidPage(
-  selectInput(inputId = "species",
-              label = "Species",
-              choices = NEesp::species_key$Species),
   
-  selectInput(inputId = "epu",
-              label = "EPU",
-              choices = c("GB", "GOM", "MAB")),
+  h1("Generate Northeast ESP Preliminary Reports"),
   
-  selectInput(inputId = "region",
-              label = "Stock Region",
-              choices = sort(unique(NEesp::regression_species_regions$Region))),
-  
-  selectInput(inputId = "lag",
-              label = "Lag correlation? \n(for predictive potential)",
-              choices = 0:10),
-  
-  selectInput(inputId = "remove",
-              label = "Remove past 10 years of data? \n(for predictive potential)",
-              choices = c("FALSE", "TRUE")),
-  
-  downloadButton("report", "Generate report")
+  navlistPanel(
+    
+    widths = c(2, 10),
+    
+    tabPanel(
+      
+      "Regression Reports",
+      
+      splitLayout(
+
+        verticalLayout(
+          h2("Report parameters"),
+
+          selectInput(
+            inputId = "species",
+            label = "Species",
+            choices = NEesp::species_key$Species
+          ),
+
+          selectInput(
+            inputId = "epu",
+            label = "EPU",
+            choices = c("GB", "GOM", "MAB")
+          ),
+
+          selectInput(
+            inputId = "region",
+            label = "Stock Region",
+            choices = sort(unique(NEesp::regression_species_regions$Region))
+          ),
+
+          selectInput(
+            inputId = "lag",
+            label = "Lag correlation by how many years? \n(for predictive potential)",
+            choices = 0:10
+          ),
+
+          selectInput(
+            inputId = "remove",
+            label = "Remove past 10 years of data? \n(for predictive potential)",
+            choices = c("FALSE", "TRUE")
+          ),
+
+          downloadButton("report", "Generate regression report")
+        ),
+
+        verticalLayout(
+          h2("Possible species - region - EPU combinations"),
+          DT::dataTableOutput("table",
+            height = "100%"
+          )
+        ),
+        cellWidths = c("40%", "60%"),
+        cellArgs = list(style = "overflow-x: hidden;")
+      )
+    ),
+    
+    tabPanel(
+      
+      "Indicator Reports",
+      
+      h2("Report parameters"),
+      
+      selectInput(
+        inputId = "ind_species",
+        label = "Species",
+        choices = NEesp::species_key$Species
+      ),
+      
+      downloadButton("ind_report", "Generate indicator report")
+    )
+  )
 )

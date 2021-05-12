a9e097ec54392db99b77706694b68b8c1a55e80b
Abigail <abigail.tyrell@noaa.gov>
2021-05-12 14:02:09 -0400

    working on esp skeleton functions

--- R/server.R
+++ R/server.R
@@ -28,7 +28,7 @@ server <- function(input, output, session) {
 
   observeEvent(input$go, {
 
-    # rendering message 
+    # rendering message
     id <- showNotification(
       "Rendering report...",
       duration = NULL,
@@ -274,31 +274,31 @@ server <- function(input, output, session) {
   ),
   server = FALSE
   )
-  
+
   # stock-indicator analysis ----
-  
+
   ## display graph ----
   observeEvent(input$go3, {
-    
-    # rendering message 
+
+    # rendering message
     id <- showNotification(
       "Rendering report...",
       duration = NULL,
       closeButton = FALSE,
       type = "message"
     )
-    
+
     on.exit(removeNotification(id), add = TRUE)
 
     # show report
-    
-    if(input$si_pattern == "ex: north, south, fall..."){
+
+    if (input$si_pattern == "ex: north, south, fall...") {
       new_pattern <- NULL
       new_remove <- NULL
     } else {
       new_pattern <- input$si_pattern %>%
         stringr::str_split(pattern = ", ")
-      
+
       new_remove <- input$si_remove %>%
         stringr::str_split(pattern = ", ")
     }
@@ -313,79 +313,119 @@ server <- function(input, output, session) {
         species = input$si_species,
         mode = "shiny"
       )
-      
     })
-    
-    output$add_to_rpt <- renderUI({actionButton("go4", "Add to report")})
-    output$download_rpt <- renderUI({downloadButton("go5", "Download report")})
+
+    output$add_to_rpt <- renderUI({
+      actionButton("go4", "Add to report")
+    })
+    output$text <- renderUI({
+      textOutput("txt")
+    })
+    output$download_rpt <- renderUI({
+      downloadButton("go5", "Download report")
+    })
   })
-  
+
   ## add to report ----
   observeEvent(input$go4, {
-    
-    # rendering message 
+
+    # rendering message
     id <- showNotification(
       "Adding to report...",
       duration = NULL,
       closeButton = FALSE,
       type = "message"
     )
-    
+
     on.exit(removeNotification(id), add = TRUE)
 
     # must set file path, pattern (or null), remove (or null), metric, and lag
     # set species when rendering rmd
-    path <- paste(tempdir(), "BOOK", "body.Rmd", sep = "/")
-    if(!file.exists(path)){
-      dir.create(paste(tempdir(), "BOOK", sep = "/"))
+    path <- paste(tempdir(), "/SI-BOOK/",
+      input$si_file$name %>% stringr::str_remove(".csv"),
+      ".Rmd",
+      sep = ""
+    )
+    # clear file and re-write if it already exists
+    if (file.exists(path)) {
+      file.remove(path)
+    }
+
+    if (!file.exists(path)) {
+      dir.create(paste(tempdir(), "SI-BOOK", sep = "/"))
       file.create(path)
       first <- c()
-    } else {first <- readLines(con = path) %>%
-      paste(collapse = "\n") 
+    } else {
+      first <- readLines(con = path) %>%
+        paste(collapse = "\n")
     }
-    
-    if(input$si_pattern == "ex: north, south, fall..."){
+
+    if (input$si_pattern == "ex: north, south, fall...") {
       new_pattern <- toString("NULL")
       new_remove <- toString("NULL")
     } else {
       new_pattern <- input$si_pattern %>%
         stringr::str_split(pattern = ", ")
-      
+
       new_remove <- input$si_remove %>%
         stringr::str_split(pattern = ", ")
     }
-    
-    print(input$si_file$datapath %>% stringr::str_replace_all("\\\\", "/"))
-    
-    writeLines(text = paste(first, "\n\n",
-                            "```{r}\n",
-                            "file_name <- '", input$si_file$name, "'\n",
-                            "file <- '", input$si_file$datapath %>% stringr::str_replace_all("\\\\", "/"), "'\n",
-                            "met <- '", input$si_metric, "'\n",
-                            "pat <- ", new_pattern, "\n",
-                            "rem <- ", new_remove, "\n",
-                            "lag <- ", as.numeric(input$si_lag), "\n",
-                            "res <- knitr::knit_child(
+
+    # print(input$si_file$datapath %>% stringr::str_replace_all("\\\\", "/"))
+
+    writeLines(
+      text = paste(first, "\n\n",
+        "```{r}\n",
+        "file_name <- '", input$si_file$name, "'\n",
+        "file <- '", input$si_file$datapath %>% stringr::str_replace_all("\\\\", "/"), "'\n",
+        "met <- '", input$si_metric, "'\n",
+        "pat <- ", new_pattern, "\n",
+        "rem <- ", new_remove, "\n",
+        "lag <- ", as.numeric(input$si_lag), "\n",
+        "res <- knitr::knit_child(
                             text = knitr::knit_expand(
                             system.file('summary_esp_template/child.Rmd', package = 'NEesp')
                             ),
                             quiet = TRUE
                             )
                             cat(res, sep = '\\n\\n')", "\n",
-                            "```",
-                            sep = ""),
-               con = path
+        "```",
+        sep = ""
+      ),
+      con = path
+    )
+
+    data_added <- list.files(
+      path = paste(tempdir(), "SI-BOOK", sep = "/"),
+      pattern = ".Rmd"
+    )
+    print(data_added)
+    if ("intro.Rmd" %in% data_added) {
+      data_added <- data_added[-which(data_added == "intro.Rmd")]
+    }
+    if ("end.Rmd" %in% data_added) {
+      data_added <- data_added[-which(data_added == "end.Rmd")]
+    }
+    if ("report.Rmd" %in% data_added) {
+      data_added <- data_added[-which(data_added == "report.Rmd")]
+    }
+    output$txt <- renderText({
+      paste("Data added to report:",
+        paste(data_added, collapse = "\n") %>%
+          stringr::str_remove(".Rmd"),
+        sep = "\n\n"
       )
     })
-  
+  })
+
   ## download report ----
   output$go5 <- downloadHandler(
-    
+
     # create file name
     filename = function() {
       paste(input$si_species, "_esp_skeleton.zip", sep = "")
     },
-    
+
     # create file content
     content = function(file) {
 
@@ -396,51 +436,83 @@ server <- function(input, output, session) {
         closeButton = FALSE,
         type = "message"
       )
-      
+
       on.exit(removeNotification(id), add = TRUE)
-      
+
       # create rmd file
-      intro <- readLines(system.file('summary_esp_template/intro.Rmd', package = 'NEesp')) %>%
+      # beginning and end
+      intro <- readLines(system.file("summary_esp_template/intro.Rmd", package = "NEesp")) %>%
         paste(collapse = "\n")
-      body <- readLines(paste(tempdir(), "BOOK", "body.Rmd", sep = "/")) %>%
+      end <- readLines(system.file("summary_esp_template/end.Rmd", package = "NEesp")) %>%
         paste(collapse = "\n")
-      end <- readLines(system.file('summary_esp_template/end.Rmd', package = 'NEesp')) %>%
-        paste(collapse = "\n")
-      
-      path2 <- paste(tempdir(), "BOOK", "report.Rmd", sep = "/")
+
+      # body
+      data_added <- list.files(
+        path = paste(tempdir(), "SI-BOOK", sep = "/"),
+        full.names = TRUE,
+        pattern = ".Rmd"
+      )
+      print(data_added)
+
+      if (stringr::str_detect(toString(data_added), "intro.Rmd")) {
+        data_added <- data_added[-stringr::str_which(data_added, "intro.Rmd")]
+      }
+      if (stringr::str_detect(toString(data_added), "end.Rmd")) {
+        data_added <- data_added[-stringr::str_which(data_added, "end.Rmd")]
+      }
+      print(stringr::str_detect(toString(data_added), "report.Rmd"))
+      if (stringr::str_detect(toString(data_added), "report.Rmd")) {
+        data_added <- data_added[-stringr::str_which(data_added, "report.Rmd")]
+      }
+      print("BAD FILES REMOVED")
+      print(data_added)
+      body <- ""
+      for (i in data_added) {
+        print(i)
+        body <- paste(body,
+          readLines(i) %>%
+            paste(collapse = "\n"),
+          sep = "\n\n"
+        )
+      }
+
+      path2 <- paste(tempdir(), "SI-BOOK", "report.Rmd", sep = "/")
+      # clear file and re-write if it already exists
+      if (file.exists(path2)) {
+        file.remove(path2)
+      }
       file.create(path2)
-      writeLines(text = paste(intro, body, end, sep = "\n\n"),
-                 con = path2)
-      
+      writeLines(
+        text = paste(intro, body, end, sep = "\n\n"),
+        con = path2
+      )
+
       # knit rmd file
-      rmarkdown::render(input = path2,
-                        params = list(species = input$si_species),
-                        envir = new.env() # render in clean env or else `file` variable will mess up download later
-                        )
+      rmarkdown::render(
+        input = path2,
+        params = list(species = input$si_species),
+        envir = new.env() # render in clean env or else `file` variable will mess up download later
+      )
 
       # zip files
       files2zip <- c(
         list.files(
-          path = paste(tempdir(), "BOOK", sep = "/"),
+          path = paste(tempdir(), "SI-BOOK", sep = "/"),
           full.names = TRUE,
           recursive = TRUE,
           pattern = "report.doc"
         ),
-        list.files(
-          path = paste(tempdir(), "BOOK", sep = "/"),
-          full.names = TRUE,
-          recursive = TRUE,
-          pattern = ".png"
-        )
+        paste(tempdir(), "SI-BOOK", "figures", sep = "/")
       )
 
-      utils::zip(paste(tempdir(), "BOOK", "testZip", sep = "/"),
-                 files = files2zip,
-                 flags = "-j"
+      # can't have figures folder without changing working directory or listing all parent directories up to ~
+      utils::zip(paste(tempdir(), "SI-BOOK", "testZip", sep = "/"),
+        files = files2zip,
+        flags = "-j"
       )
-      
+
       file.copy(
-        from = paste(tempdir(), "BOOK", "testZip.zip", sep = "/"),
+        from = paste(tempdir(), "SI-BOOK", "testZip.zip", sep = "/"),
         to = file
       )
     },
--- R/ui.R
+++ R/ui.R
@@ -183,13 +183,13 @@ ui <- fluidPage(
         cellArgs = list(style = "overflow-x: hidden;")
       )
     ),
-    
+
     # stock-indicator analysis ----
     tabPanel(
       "Stock-Indicator Analysis",
-      
+
       h2("Stock-Indicator Analysis"),
-      
+
       h3("Select your input file"),
       p("This should be a .csv outputted from the 'Download a Regression Report' tab."),
       fileInput(
@@ -198,22 +198,22 @@ ui <- fluidPage(
         multiple = FALSE,
         accept = ".csv"
       ),
-      
+
       h3("Select your parameters"),
-      
+
       selectInput(
         inputId = "si_species",
         label = "Species",
         choices = NEesp::species_key$Species,
         selected = "Black sea bass"
       ),
-      
+
       selectInput(
         inputId = "si_metric",
         label = "Stock metric",
         choices = c("Recruitment", "Abundance", "Catch", "Fmort")
       ),
-      
+
       selectInput(
         inputId = "si_lag",
         label = "Number of years by which stock data was lagged",
@@ -225,19 +225,21 @@ ui <- fluidPage(
         label = "Are there any patterns that should be filtered from the `Var` column? Separate multiple entries with a comma",
         value = "ex: north, south, fall..."
       ),
-      
+
       textInput(
         inputId = "si_remove",
         label = "Should patterns be removed (TRUE) or retained (FALSE)? Separate multiple entries with a comma",
         value = "ex: TRUE, TRUE, FALSE..."
       ),
-      
+
       actionButton("go3", "click"),
       p(""),
       uiOutput("add_to_rpt"),
       p(""),
+      uiOutput("text"),
+      p(""),
       uiOutput("download_rpt"),
-      
+
       h4(""),
       plotOutput("stock_indicator", height = "800px")
     )
